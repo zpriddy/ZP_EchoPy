afe432282fe91c4a495e0dd34edf36c3ca618d2a
Zachary Priddy <me@zpriddy.com>
2015-07-08 17:28:41 -0700

    More Testing
    Added setup for Nest Control

--- echopy_helloworld.py
+++ echopy_helloworld.py
@@ -12,8 +12,9 @@ def data_handler(rawdata):
 	global MyDataStore
 	#print rawdata['session']
 	currentSession = MyDataStore.getSession(rawdata['session'])
+	currentUser = MyDataStore.getUser(rawdata['session'])
 	currentRequest = rawdata['request']
-	response = request_handler(currentSession, currentRequest)
+	response = request_handler(currentSession, currentUser, currentRequest)
 
 
 	#print json.dumps({"version":appVersion,"response":response},sort_keys=True,indent=4)
@@ -21,16 +22,16 @@ def data_handler(rawdata):
 	return json.dumps({"version":appVersion,"response":response},indent=2,sort_keys=True)
 
 
-def request_handler(session, request):
+def request_handler(session, user, request):
 	requestType = request['type']
 	
 	if requestType == "LaunchRequest":
-		return launch_request(session, request)
+		return launch_request(session, user, request)
 	elif requestType == "IntentRequest":
-		return intent_request(session,request)
+		return intent_request(session, user, request)
 
 
-def launch_request(session, request):
+def launch_request(session, user, request):
 	output_speech = "Welcome to the Alexa Skills Kit, you can say hello"
 	output_type = "PlainText"
 
@@ -42,7 +43,7 @@ def launch_request(session, request):
 
 	return response
 
-def intent_request(session,request):
+def intent_request(session, user, request):
 	print "intent_request"
 	if request['intent']['name'] == "HelloWorldIntent":
 		output_speech = "Hellow World!"
@@ -55,8 +56,23 @@ def intent_request(session,request):
 		response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':True}
 
 		return response
+	'''
+	elif request['intent']['name'] ==  "NestSetIntent":
+		nestTempValue = request['intent']['slots']['temp']['value']
+		output_speech = "Telling Nest to cool to " + str(nestTempValue) + " degrees fahrenheit"
+		output_type = "PlainText"
+
+		card_type = "Simple"
+		card_title = "HelloWorld - Setting Nest Temp"
+		card_content = "Telling Nest to cool to " + str(nestTempValue) + " degrees fahrenheit."
+
+		response = {"outputSpeech": {"type":output_type,"text":output_speech},"card":{"type":card_type,"title":card_title,"content":card_content},'shouldEndSession':True}
+
+		return response
+	
+		'''
 	else:
-		return launch_request(session, request) ##Just do the same thing as launch request
+		return launch_request(session, user, request) ##Just do the same thing as launch request
 
 
 
@@ -70,12 +86,29 @@ class Session:
 	def getSessionID(self):
 		return self.sessionId
 
+class User:
+	def __init__(self,userId):
+		self.userId = userId
+		self.settings = {}
+
+	def getUserId(self):
+		return self.userId
+
 class DataStore:
 	def __init__(self):
 		self.sessions = {}
+		self.users = {}
 
 	def getSession(self,session):
 		if session['new'] is True or session['sessionId'] not in self.sessions:
 			self.sessions[session['sessionId']] = Session(session)
 
 		return self.sessions[session['sessionId']]
+
+	def getUser(self,session):
+		userId = session['user']['userId']
+		if userId not in self.users:
+			self.users[userId] = User(userId)
+
+		return self.users[userId]
+
